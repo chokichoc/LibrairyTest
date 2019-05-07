c7830527f001e2b1faf263431e0d7cd131e616e1
Selim Abbas Said <selim093@hotmail.fr>
2019-05-07 17:26:42 +0200

    Turn to library again and change activity name

--- app/src/main/java/com/example/librairytest/TestActivity.kt
+++ app/src/main/java/com/example/librairytest/TestActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
